9de04dfd095b1b698f76a022a7b30547b56414de
Timo Klüber <timo.klueber@googlemail.com>
2019-10-07 14:32:32 +0200

    first login successful

--- frontend/lib/screens/HomeScreen.dart
+++ frontend/lib/screens/HomeScreen.dart
@@ -1,6 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  Future<void> _signInAnonymously() async {
+    try {
+      final FirebaseUser user = (await FirebaseAuth.instance
+              .signInWithEmailAndPassword(
+                  email: 'timo@klueber.email', password: '12345678'))
+          .user;
+
+      print(user.email);
+    } catch (e) {
+      print(e); // TODO: show dialog with error
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +22,16 @@ class HomeScreen extends StatelessWidget {
         title: Text("Baloise expenses"),
       ),
       body: Center(
-        child: Text("Baloise expenses"),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: _signInAnonymously,
+              child: Text("Anonymes Login"),
+            ),
+            Text("Baloise expenses"),
+            Text("USER"),
+          ],
+        ),
       ),
     );
   }
