903d0bd5f4bd123e53440587ecffcda9e265f263
Timo Klüber <timo.klueber@googlemail.com>
2019-10-08 09:30:30 +0200

    upload file

--- frontend/lib/screens/HomeScreen.dart
+++ frontend/lib/screens/HomeScreen.dart
@@ -1,8 +1,22 @@
 import 'package:balexpenses/screens/AuthScreen.dart';
 import 'package:balexpenses/screens/InvoiceSelection.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  UserInfo user;
+
+  void _setUser(u) {
+    setState(() {
+      this.user = u;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,8 +25,13 @@ class HomeScreen extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            AuthScreen(),
-            InvoiceSelection(),
+            AuthScreen(
+              setUser: _setUser,
+            ),
+            InvoiceSelection(
+              user: user,
+            ),
+            Text(this.user != null ? user.email : '-'),
           ],
         ));
   }
--- frontend/lib/screens/InvoiceSelection.dart
+++ frontend/lib/screens/InvoiceSelection.dart
@@ -1,23 +1,28 @@
 import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-class InvoiceSelection extends StatefulWidget {
-  @override
-  _InvoiceSelectionState createState() => _InvoiceSelectionState();
-}
+class InvoiceSelection extends StatelessWidget {
+  final UserInfo user;
 
-class _InvoiceSelectionState extends State<InvoiceSelection> {
+  InvoiceSelection({this.user});
+
   File _image;
+  final FirebaseStorage _storage =
+      FirebaseStorage(storageBucket: 'gs://balexpenses-bbaae.appspot.com/');
+
+  StorageUploadTask _uploadTask;
 
   Future getImage() async {
-    print("getImage");
-    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+    _image = await ImagePicker.pickImage(source: ImageSource.gallery);
+  }
 
-    setState(() {
-      _image = image;
-    });
+  void _startUpload() {
+    String filePath = 'invoices/${user.uid}/nase.jpg';
+    _storage.ref().child(filePath).putFile(_image);
   }
 
   @override
@@ -25,25 +30,29 @@ class _InvoiceSelectionState extends State<InvoiceSelection> {
     return Container(
       child: Column(
         children: <Widget>[
-          Container(
-            width: 150,
-            height: 100,
-            decoration:
-                BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
-            child: this._image != null
-                ? Image.file(
-                    _image,
-                    fit: BoxFit.cover,
-                    width: double.infinity,
-                  )
-                : Text(
-                    'No Image Selected',
-                    textAlign: TextAlign.center,
-                  ),
-          ),
+//          Container(
+//            width: 150,
+//            height: 100,
+//            decoration:
+//                BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+//            child: this._image != null
+//                ? Image.file(
+//                    _image,
+//                    fit: BoxFit.cover,
+//                    width: double.infinity,
+//                  )
+//                : Text(
+//                    'No Image Selected',
+//                    textAlign: TextAlign.center,
+//                  ),
+//          ),
           RaisedButton(
             onPressed: getImage,
             child: Text("select image from gallery"),
+          ),
+          RaisedButton(
+            onPressed: _startUpload,
+            child: Text("upload dummy"),
           )
         ],
       ),
