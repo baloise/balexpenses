69b683c018d618d130da6c0acb8ff13cc716162c
Timo Klüber <timo.klueber@googlemail.com>
2019-10-08 21:57:43 +0200

    print uid and filebucket

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,11 +4,7 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request:any, response:any) => {
-    response.send("Hello from Firebase!");
-});
-
-exports.scanReceipt = functions.storage.object().onFinalize((object: any) => {
+exports.onUploadedObject = functions.storage.object().onFinalize((object: any) => {
     // Determine the bucket and path that triggered us
     const fileBucket = object.bucket;
     const filePath = object.name;
@@ -19,22 +15,4 @@ exports.scanReceipt = functions.storage.object().onFinalize((object: any) => {
 
     console.log('--->' + uid + ' ' + fileName);
     console.log('fileBucket' + fileBucket);
-
-    // // Call Cloud Vision to find text in the receipt
-    // const visionClient = new vision.ImageAnnotatorClient();
-    // return visionClient.textDetection(`gs://${fileBucket}/${filePath}`).then(([result]) => {
-    //     const detections = result.textAnnotations;
-    //
-    //     // Find the total amount in this receipt
-    //     const amount = receipt.findTotal(detections);
-    //
-    //     // Determine the document to write the amount from this receipt to
-    //     let expenseDoc = admin.firestore().doc(`users/${uid}/expenses/${fileName}`);
-    //
-    //     // Write the amount to this new document
-    //     return expenseDoc.set({
-    //         created_at: admin.firestore.FieldValue.serverTimestamp(),
-    //         item_cost: amount
-    //     });
-    // });
 });
