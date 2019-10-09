24589db96957b81c8b012f2bc25e6d34a473e011
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2019-10-09 16:15:20 +0200

    pass invoice date to saveOcr function

--- frontend/lib/providers/ocr_service.dart
+++ frontend/lib/providers/ocr_service.dart
@@ -10,16 +10,18 @@ class OcrService with ChangeNotifier {
   // credits: https://medium.com/@teresa.wu/googles-ml-kit-text-recognition-with-sample-app-of-receipts-reading-7fe6dc68ada3
   Future<Invoice> scanInvoice(File image) async {
     FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(image);
-    final TextRecognizer textRecognizer = FirebaseVision.instance.textRecognizer();
-    final VisionText visionText = await textRecognizer.processImage(visionImage);
+    final TextRecognizer textRecognizer = FirebaseVision.instance
+        .textRecognizer();
+    final VisionText visionText = await textRecognizer.processImage(
+        visionImage);
 
     List<double> allNumbers = findNumbers(visionText.text);
     double maxNumber = -1;
     if (allNumbers != null && allNumbers.isNotEmpty) {
       maxNumber = allNumbers.reduce(max);
     }
-    return Invoice(maxNumber);
-
+    var invoiceDate = findDate(visionText.text);
+    return Invoice(maxNumber, invoiceDate);
   }
 
   List<double> findNumbers(String text) {
@@ -29,10 +31,35 @@ class OcrService with ChangeNotifier {
       Iterable<RegExpMatch> matches = exp.allMatches(text);
 
       result = matches
-      .map( (match) { return text.substring(match.start, match.end).replaceAll(",", ".");})
-      .where((str) {return str.contains(".");})
-      .map( (str) { return double.parse(str);}).toList()
+          .map((match) {
+        return text.substring(match.start, match.end).replaceAll(",", ".");
+      })
+          .where((str) {
+        return str.contains(".");
+      })
+          .map((str) {
+        return double.parse(str);
+      }).toList()
       ;
+    }
+    return result;
+  }
+
+  DateTime findDate(String text) {
+    DateTime result;
+    if (text != null && text.isNotEmpty) {
+      RegExp exp = new RegExp(r"([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{2,4})");
+      Iterable<RegExpMatch> matches = exp.allMatches(text);
+
+      var match = matches.first;
+     if (match != null) {
+        var g1 = match.group(1);
+        var g2 = match.group(2);
+        var g3 = match.group(3);
+        int year = int.parse(g3);
+        year = (year < 100) ? year + 2000 : year;
+        result = DateTime(year, int.parse(g2), int.parse(g1));
+      }
     }
     return result;
   }
