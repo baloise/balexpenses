503ae64a6377607d399f41c16ce832d91e33ca55
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2019-10-11 10:53:12 +0200

    more styling

--- frontend/lib/screens/ResultItem.dart
+++ frontend/lib/screens/ResultItem.dart
@@ -2,12 +2,23 @@ import 'package:flutter/material.dart';
 
 class ResultItem extends StatelessWidget {
 
-  final String content;
+  final String label;
+  final String value;
 
-  ResultItem(this.content);
+  ResultItem(this.label, this.value);
 
   @override
   Widget build(BuildContext context) {
-    return Text("XX $content");
+    return Card(
+      color: Colors.green,
+      child: ListTile(
+        trailing: Icon(Icons.keyboard_arrow_right),
+        title: Text(
+            value,
+          style: TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        subtitle: Text(label, style: TextStyle(color: Colors.white)),
+      ),
+    );
   }
 }
--- frontend/lib/screens/ResultScreen.dart
+++ frontend/lib/screens/ResultScreen.dart
@@ -29,14 +29,19 @@ class ResultScreen extends StatelessWidget {
                       children: <Widget>[
                         Text(
                             (user != null)
-                                ? "UserId: ${snapshot.data.uid}"
-                                : "UserId: -"
+                                ? snapshot.data.email
+                                : "No User",
+                          style: TextStyle(fontSize: 20),
                         ),
-                        Column(
-                          children: <Widget>[
-                            ...(state.result.map((it) =>
-                                ResultItem("${it.key}=${it.value}")).toList()),
-                          ],
+                        Expanded(
+                          child: ListView.builder(
+                            scrollDirection: Axis.vertical,
+                            itemCount: state.result.length,
+                            itemBuilder: (context, index) {
+                              var it = state.result.elementAt(index);
+                              return ResultItem(it.key, it.value.toString());
+                            },
+                          ),
                         ),
                         RaisedButton(
                           child: Text('get Result'),
@@ -49,89 +54,7 @@ class ResultScreen extends StatelessWidget {
             },
           );
 
-          return Center(
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    (user != null) ? "UserId: ${snapshot.data.uid}" : "UserId: -"
-                  ),
-                  Consumer<StateProvider>(
-                    builder: (ctx, state, child) {
-                      return Column(
-                        children: <Widget>[
-                          ...(state.result.map((it) => ResultItem("${it.key}=${it.value}")).toList()),
-                        ],
-                      );
-                    },
-                  ),
-                  RaisedButton(
-                    child: Text('get Result'),
-                    onPressed: () => stateProvider.getResult(user),
-                  ),
-                ],
-              )
-            ),
-          );
-        },
-      ),
-    );
-  }
-  Widget build2(BuildContext context) {
-    final StateProvider stateProvider = Provider.of<StateProvider>(context);
-    final FirebaseAuthService auth = Provider.of<FirebaseAuthService>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Results Screen"),
-      ),
-      body: StreamBuilder<User>(
-        stream: auth.onAuthStateChanged,
-        builder: (_, AsyncSnapshot<User> snapshot) {
-          User user;
-          if (snapshot.connectionState == ConnectionState.active && snapshot.data != null) {
-            user = snapshot.data;
-          }
-          return Center(
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    (user != null) ? "UserId: ${snapshot.data.uid}" : "UserId: -"
-                  ),
-                  RaisedButton(
-                    child: Text('get Result'),
-                    onPressed: () => stateProvider.getResult(user),
-                  ),
-              Consumer<StateProvider>(
-                builder: (ctx, state, child) {
-                  return ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    itemCount: state.result.length,
-                    itemBuilder: (context, index) {
-                      return ResultItem([state.result.elementAt(index)].map((it) => "${it.key}=${it.value}").first);
-//                      return ListTile(
-//                        title: ResultItem([state.result.elementAt(index)].map((it) => "${it.key}=${it.value}").first),
-//                      );
-                    },
-                  );
-                },
-              ),
-/*
-                  Consumer<StateProvider>(
-                    builder: (ctx, state, child) {
-                      return Column(
-                        children: <Widget>[
-                          ...(state.result.map((it) => ResultItem("${it.key}=${it.value}")).toList()),
-                        ],
-                      );
-                    },
-                  ),
-*/
 
-                ],
-              )
-            ),
-          );
         },
       ),
     );
